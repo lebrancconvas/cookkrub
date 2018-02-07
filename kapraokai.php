a7ec0ec1d74a26d44a919b87e343d714dcc96117
Lebranc Convas <lebrancconvas@gmail.com>
2018-02-08 02:50:47 +0700

    Initial Commit

---
+++ kapraokai.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>ข้าวผัดกะเพราไก่ไข่ดาว | Cookkrub Menu</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="node_modules/bootstrap/dist/css/bootstrap.min.css" />
+    
+</head>
+<body>
+    <div class="jumbotron">
+        <h1 class="display-4">ข้าวผัดกะเพราไก่ไข่ดาว</h1>
+        <p class="lead">แน่นอนว่าหากคุณหลงไหลในเมนูนี้แล้ว คุณจะถอนตัวจากมันไม่ขึ้นเลย แล้วยังไงต่อล่ะ? <br> หลังจากนั้นเราก็จะมารังสรรค์สุดยอดนี้กันยังไงล่ะ</p>
+    </div>
+    
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+</body>
+</html>
