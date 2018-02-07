baca9ca7742dd3dbe59a07bb050ea5214f1ae53f
Lebranc Convas <lebrancconvas@gmail.com>
2018-02-08 04:04:02 +0700

    Edit Jumbotron + Carousel

--- index.php
+++ index.php
@@ -3,14 +3,16 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="refresh" content="3" >
+    <meta http-equiv="refresh" content="120" >
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Cookkrub | Club for Cooking-Lover</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="node_modules/bootstrap/dist/css/bootstrap.min.css" />
     
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<!--Navbar Section-->
+<nav class="navbar navbar-expand-lg navbar-success bg-light">
   <a class="navbar-brand" href="index.php">Cookkrub</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -19,17 +21,17 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="menulist.php">List of Menu</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="contributor.php">Contributor</a>
           <a class="dropdown-item" href="#">Action2</a>
           <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="#">Something else here</a>
@@ -45,21 +47,30 @@
     </form>
   </div>
 </nav>
+<!--Carousel (Image Slide)-->
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=495705ce4fffc406c4c9fc09b13a6db9&auto=format&fit=crop&w=750&h=200&q=80" alt="First slide">
+      <img class="d-block w-100" src="https://images.unsplash.com/photo-1493770348161-369560ae357d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=495705ce4fffc406c4c9fc09b13a6db9&auto=format&fit=crop&w=1280&h=400&q=80" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://images.unsplash.com/photo-1481070414801-51fd732d7184?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=82203e4d57fc0d3bdd8ffc0f66d09763&auto=format&fit=crop&w=750&h=200&q=80" alt="Second slide">
+      <img class="d-block w-100" src="https://images.unsplash.com/photo-1481070414801-51fd732d7184?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=82203e4d57fc0d3bdd8ffc0f66d09763&auto=format&fit=crop&w=1280&h=400&q=80" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://images.unsplash.com/photo-1506354666786-959d6d497f1a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=86c8c1fd5e9e5b384696472a095c42ac&auto=format&fit=crop&w=750&h=200&q=80" alt="Third slide">
+      <img class="d-block w-100" src="https://images.unsplash.com/photo-1506354666786-959d6d497f1a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=86c8c1fd5e9e5b384696472a095c42ac&auto=format&fit=crop&w=1280&h=400&q=80" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://images.unsplash.com/photo-1505685509042-16b1b75030cf?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3c004629f0583dabb2acb6404791c4d9&auto=format&fit=crop&w=1280&h=400&q=80" alt="Fourth slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://images.unsplash.com/reserve/tHTHup3YTN6XsLwf43vY_IMG_8003.jpg?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f5129da3324e4a10f3fd821feffdc3fc&auto=format&fit=crop&w=1280&h=400&q=80" alt="Fifth slide">
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
@@ -70,6 +81,17 @@
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
+</div>
+<!--Jumbotron Section-->
+<div class="jumbotron">
+  <div class="container text-center">
+    <h1><i class="fa fa-hand-peace-o" style="font-size:48px;color:green"></i>&nbsp;COOKKRUB</h1> 
+    <p>หากคุณรักในการทำอาหาร | คุณมาถูกที่แล้ว :)</p> 
+  </div>
+  </div>
+<div class="container">
+  <p>This is some text.</p> 
+  <p>This is another text.</p> 
 </div>
 
     <script src="node_modules/jquery/dist/jquery.min.js"></script>
