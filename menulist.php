187662d0b3dde64b984a811860c4ef0e3a802a0e
Lebranc Convas <lebrancconvas@gmail.com>
2018-02-08 02:51:28 +0700

    Add Menu List.

--- menulist.php
+++ menulist.php
@@ -9,7 +9,18 @@
     
 </head>
 <body>
-    
+<ul class="list-group">
+    <li class="list-group-item"><button type="button" class="btn btn-outline-success my-2 my-sm-0"><a class="nav-link" href="index.php">Back</a></button></li>
+    <li class="list-group-item list-group-item-primary"><button type="button" class="btn btn-outline-light my-2 my-sm-0"><a class="nav-link" href="kapraokai.php">ข้าวผัดกะเพราไก่ไข่ดาว</a></button></li>
+    <li class="list-group-item list-group-item-secondary">This is a secondary list group item</li>
+    <li class="list-group-item list-group-item-success">This is a success list group item</li>
+    <li class="list-group-item list-group-item-danger">This is a danger list group item</li>
+    <li class="list-group-item list-group-item-warning">This is a warning list group item</li>
+    <li class="list-group-item list-group-item-info">This is a info list group item</li>
+    <li class="list-group-item list-group-item-light">This is a light list group item</li>
+    <li class="list-group-item list-group-item-dark">This is a dark list group item</li>
+</ul>
+
     <script src="node_modules/jquery/dist/jquery.min.js"></script>
     <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
